cc5a65d7326ee6960c5bc5b42ef1c482e902f102
Zhigang Fang <zhigang1992@gmail.com>
2016-11-13 15:20:04 +0800

    Make it carthage compatiable

---
+++ MTDates/MTDates.h
@@ -0,0 +1,19 @@
+//
+//  MTDates.h
+//  MTDates
+//
+//  Created by Zhigang Fang on 11/13/16.
+//  Copyright © 2016 Mysterious Trousers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MTDates.
+FOUNDATION_EXPORT double MTDatesVersionNumber;
+
+//! Project version string for MTDates.
+FOUNDATION_EXPORT const unsigned char MTDatesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MTDates/PublicHeader.h>
+
+
